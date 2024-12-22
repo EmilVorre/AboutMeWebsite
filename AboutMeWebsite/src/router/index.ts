680af3cc8f24f076191438f1402e34802e342371
VorHerre <evorre23@student.aau.dk>
2024-12-22 10:21:16 +0100

    update routing to set AboutMe as the home component and add project routes

--- AboutMeWebsite/src/router/index.ts
+++ AboutMeWebsite/src/router/index.ts
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import type { RouteRecordRaw } from 'vue-router'
 import ProjectList from '../components/ProjectList.vue'
 import ProjectDetail from '../components/ProjectDetail.vue'
+import AboutMe from '../components/AboutMe.vue'
 
-const routes = [
-  { path: '/', component: ProjectList },
-  { path: '/project/:id', component: ProjectDetail, props: true }
+const routes: Array<RouteRecordRaw> = [
+  { path: '/', component: AboutMe },
+  { path: '/projects', component: ProjectList },
+  { path: '/project/:id', name: 'ProjectDetail', component: ProjectDetail, props: true }
 ]
 
 const router = createRouter({
